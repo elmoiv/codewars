26820d8f91fce1e0ffe77f641128c9a77fa351c2
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-09-04 17:33:05 +0200

    Create Next smaller number with the same digits.py

---
+++ 4 kyu/Next smaller number with the same digits.py
@@ -0,0 +1,10 @@
+def next_smaller(n):
+	s = str(n)
+	for i in range(-1,-len(s),-1):
+		if int(s[i-1]) > int(s[i]):
+			main, rest = ''.join(s[:i]), ''.join(sorted(s[i:],reverse=True))
+			for d in range(len(rest)):
+				if int(main[-1]) > int(rest[d]):
+					f = int(main[:-1]+rest[d]+rest[:d]+main[-1]+rest[d+1:])
+					return f if f < n and len(str(f)) == len(s) else -1
+	return -1
