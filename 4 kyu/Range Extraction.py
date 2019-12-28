ab4a02746b4bcc4f21f66cb25dad9078a33ad307
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-12-28 15:05:19 +0200

    Create Range Extraction.py

---
+++ 4 kyu/Range Extraction.py
@@ -0,0 +1,9 @@
+def solution(args):
+    l, t = [], []
+    for i in args:
+        if i + 1 in args:
+            t += [`i`]
+        else:
+            l += ['{}-{}'.format(t[0], i)] if len(t) > 1 else t + [`i`]
+            t = []
+    return ','.join(l)
