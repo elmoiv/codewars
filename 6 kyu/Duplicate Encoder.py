3c7033ab30eec71bf54e3f4c0d0253100b1607a1
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-12-30 23:47:16 +0200

    Update Duplicate Encoder.py

--- 6 kyu/Duplicate Encoder.py
+++ 6 kyu/Duplicate Encoder.py
@@ -1,2 +1,7 @@
 def duplicate_encode(word):
     return ''.join(')' if word.lower().count(i)>1 else '(' for i in word.lower())
+
+# Second hit:
+def duplicate_encode(w):
+    w = w.lower()
+    return ''.join(['(', ')'][w.count(i) > 1] for i in w)
