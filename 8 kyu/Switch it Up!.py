6c263ba72000b0aa2390fc9c68649fdd0ac42233
Khaled ElMorshedy <elmoiv@yahoo.com>
2020-12-01 19:50:03 +0200

    Create Switch it Up!.py

---
+++ 8 kyu/Switch it Up!.py
@@ -0,0 +1,1 @@
+switch_it_up=lambda number:eval(",]Tr'e'bbmnurnt['],''eNn'ieNo'h,,''tnh,g'i'E''',S'unie,v'eoSe'],n'mx,iOSw'e,i',eev'iFFe'F,['ervueo]Fe'g,[''ehe'rehnT''',''oorw,Tr'x,''uevnrOZ'S,''Eo'rTeiZ,'i["[::-1][::2])
